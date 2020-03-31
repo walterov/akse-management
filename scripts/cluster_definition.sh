787e530f22fb4270ffe03573a2f3f43e47cc0b4f
Walter Oliver <waltero@microsoft.com>
2020-03-30 17:39:19 -0700

    adding documentation and removing REPO_ROOT variable

--- scripts/cluster_definition.sh
+++ scripts/cluster_definition.sh
@@ -3,10 +3,10 @@
 set -o nounset
 set -o pipefail
 
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-cd "${REPO_ROOT}" || exit 1
+#REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+#cd "${REPO_ROOT}" || exit 1
 
-source "${REPO_ROOT}/helpers/cluster_defaults.env"
+#source "${REPO_ROOT}/helpers/cluster_defaults.env"
 
 case "$MONITORING_AZURE" in
   FALSE) cat helpers/cluster_template.tpl | envsubst > cluster_definition.json ;;
