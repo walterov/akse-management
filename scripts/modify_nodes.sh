53ce19a0bf8e4cccf21e5574f21fbd6ca040ad64
Walter Oliver <waltero@microsoft.com>
2020-06-01 21:17:11 -0700

    change modify settings and move file to helpers

--- scripts/modify_nodes.sh
+++ scripts/modify_nodes.sh
@@ -2,4 +2,10 @@
 
 # Connect with ssh to k8s master and modify settings to all VM's in in cluster
 
+# Upload ssh key to Master so that it can connect to worker nodes
+echo  "Upload ssh key file"
+scp -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${USERNAME}@${MASTER_FQDN}:~/
+
+# Execute mod_vm_settings.sh
+cat ../helpers/mod_vm_settings.sh | ssh -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${USERNAME}@${MASTER_FQDN}
 
