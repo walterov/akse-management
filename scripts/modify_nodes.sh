b7dfa99d869527ea3d9870ad60a22354d87fed1a
Walter Oliver <waltero@microsoft.com>
2020-06-01 22:43:42 -0700

    change ssh key permission after copying it

--- scripts/modify_nodes.sh
+++ scripts/modify_nodes.sh
@@ -6,6 +6,9 @@
 echo  "Upload ssh key file"
 scp -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${USERNAME}@${MASTER_FQDN}:~/.ssh
 
+# Change permissions for the ssh key
+ssh ${MASTER_FQDN} -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} -C "chmod 0600 $HOME/.ssh/${PRIVATE_KEY_FILE}"
+
 # Execute mod_vm_settings.sh on remote master node
 echo  "Execute ./helpers/mod_vm_settings.sh"
 cat ./helpers/mod_vm_settings.sh | ssh -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} ${USERNAME}@${MASTER_FQDN}
