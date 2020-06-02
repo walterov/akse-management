4826ac18696e9bbc423a8fbed0c381344021b7a5
Walter Oliver <waltero@microsoft.com>
2020-06-02 00:03:35 -0700

    change flags for modifying profile file and avoid permission denied

--- helpers/mod_vm_settings.sh
+++ helpers/mod_vm_settings.sh
@@ -28,7 +28,7 @@ do
     #TODO: Fix env variables ${USERNAME} and ${PRIVATE_KEY_FILE} they would be undefined in the remote machine
     echo "${USERNAME}@${MEMBER[${i}]}"
     echo "${HOME}/.ssh/${PRIVATE_KEY_FILE}"
-    ssh ${USERNAME}@${MEMBER[${i}]} -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -C "sudo echo 'export http_proxy=http://69.162.97.93:3128/' >> /etc/profile"
+    ssh ${USERNAME}@${MEMBER[${i}]} -i ${HOME}/.ssh/${PRIVATE_KEY_FILE} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -C "sudo echo 'export http_proxy=http://69.162.97.93:3128/' | sudo tee -a /etc/profile"
     #ssh azureuser@k8s-linuxpool-16269316-0 -i ${HOME}/.ssh/private.key.1.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -C "ls -al"
 done
 
